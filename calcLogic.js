4607a9edaf613fb1285e47e777dd74fd364286d8
timalo <tmalo@student.oulu.fi>
2021-08-30 12:52:11 +0300

    button click listeners added and started creating functions related to adding text to calc screen

--- calcLogic.js
+++ calcLogic.js
@@ -12,7 +12,6 @@ function divide(a, b) {
     return (a / b);
 }
 //----------------------------
-
 function operate(operator, num1, num2) {
     switch(operator){
         case add:
@@ -31,3 +30,30 @@ function operate(operator, num1, num2) {
             break;
     }
 }
+
+function screenWrite(symbol) { //Function that writes the number or operator on the calc screen
+    console.log(`Called write function with: ${symbol}`);
+}
+
+function readScreen(){ //function for reading the calculator screen and splitting the contents into num1, num2 and operator
+
+}
+
+//button functionality
+document.getElementById("btn0").onclick = function(){screenWrite("0")};
+document.getElementById("btn1").onclick = function(){screenWrite("1")};
+document.getElementById("btn2").onclick = function(){screenWrite("2")};
+document.getElementById("btn3").onclick = function(){screenWrite("3")};
+document.getElementById("btn4").onclick = function(){screenWrite("4")};
+document.getElementById("btn5").onclick = function(){screenWrite("5")};
+document.getElementById("btn6").onclick = function(){screenWrite("6")};
+document.getElementById("btn7").onclick = function(){screenWrite("7")};
+document.getElementById("btn8").onclick = function(){screenWrite("8")};
+document.getElementById("btn9").onclick = function(){screenWrite("9")};
+document.getElementById("btnAdd").onclick = function(){screenWrite("+")};
+document.getElementById("btnSub").onclick = function(){screenWrite("-")};
+document.getElementById("btnMult").onclick = function(){screenWrite("*")};
+document.getElementById("btnDiv").onclick = function(){screenWrite("/")};
+
+document.getElementById("btnClr").onClick = function(){screenClear()};
+document.getElementById("btnEqu").onClick = function(){readScreen()};
